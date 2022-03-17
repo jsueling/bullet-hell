35a2ed26a49a4a99f77c5c4e532e589294fed917
James Sue-Ling <james_sueling@hotmail.co.uk>
2022-03-17 14:45:56 +0000

    set up dev server

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,9 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
-   filename: 'bundle.js',
+   filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [new HtmlWebpackPlugin()]
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,9 +1,12 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
   output: {
-   filename: 'bundle.js',
+   filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [new HtmlWebpackPlugin()]
 };
