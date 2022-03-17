abe0c150e0b8cb2cb70e1e8f778b038b83c46ec3
James Sue-Ling <james_sueling@hotmail.co.uk>
2022-03-17 14:59:43 +0000

    added htmlwebpackplugin + template

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,12 +5,16 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-   filename: 'main.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
-  plugins: [new HtmlWebpackPlugin()]
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
 };
