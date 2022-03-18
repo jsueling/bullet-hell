a9c59c5324e8e4640831f489571c5ff23ce98471
James Sue-Ling <james_sueling@hotmail.co.uk>
2022-03-18 18:19:17 +0000

    basic animation framework

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import './styles.css'
 
-const canvas = document.getElementById('canvas1')
-console.log(canvas);
-const ctx = canvas.getContext('2d')
-console.log(canvas);
+let canvas
+let ctx
+
+window.addEventListener('onload', init())
+window.addEventListener('resize', () => {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+})
+
+function init() {
+  canvas = document.getElementById('canvas1')
+  ctx = canvas.getContext('2d')
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+}
+
+function animate() {
+  draw()
+  window.requestAnimationFrame(animate)
+}
+
+function draw() {
+  ctx.fillStyle = 'blue'
+  ctx.fillRect(0, 0, 100, 100)
+}
+
+animate()
